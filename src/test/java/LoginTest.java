edf0be20234c6700548d95c7ea7a52ac7cec9cf1
Dina425 <bazhenovadina@gmail.com>
2025-01-26 19:39:04 +0200

    New Project

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+  @BeforeClass
+    public void preCondition(){
+
+  }
+  @Test
+    public void loginSuccess(){
+
+    }
+    @Test
+    public void loginWrongEmail(){
+
+    }
+    @Test
+    public void loginWrongPassword(){
+
+    }
+    @AfterClass
+    public void postCondition(){
+
+    }
+}
