2cdf93e6593bf061ea128de153fd46df6d7ce1c5
Dina425 <bazhenovadina@gmail.com>
2025-03-02 18:33:02 +0200

    sd

--- src/test/java/Homework.java
+++ src/test/java/Homework.java
@@ -15,6 +15,20 @@ public class Homework {
         wd.get("https://telranedu.web.app/login");
     }
     @Test
+    public void classWork(){
+    WebElement el=wd.findElement(By.xpath("//h1/parent::*"));
+    WebElement el1=wd.findElement(By.xpath("//h1/parent::div"));
+    WebElement el2=wd.findElement(By.xpath("//h1/.."));
+
+    WebElement el3=wd.findElement(By.xpath("//h1/ancestor::*"));//all
+    WebElement el4=wd.findElement(By.xpath("//h1/ancestor::div"));//two options
+    WebElement el5=wd.findElement(By.xpath("//h1/ancestor::div[2]"));//one
+
+        WebElement el6=wd.findElement(By.xpath("//h1/ancestor-or-self::*"));
+
+
+    }
+    @Test
     public void cssLocators(){
         WebElement h1=wd.findElement(By.tagName("h1"));
         List <WebElement> listA=wd.findElements(By.tagName("a"));
@@ -32,6 +46,25 @@ public class Homework {
         WebElement el2= wd.findElement(By.xpath("//input[@placeholder='Password']"));
         WebElement el3=wd.findElement(By.xpath("//input[starts-with(@placeholder,'Pa')]"));
         WebElement el4=wd.findElement(By.xpath("//input[contains(@placeholder,'as')]"));
+        WebElement h1_2= wd.findElement(By.cssSelector("h1"));
+        WebElement h1_1= wd.findElement(By.xpath("//h1"));
+        WebElement a= wd.findElement(By.xpath("//a"));
+
+
+        WebElement el7= wd.findElement(By.className("login_login__3EHKB"));
+        WebElement el8= wd.findElement(By.cssSelector(".login_login__3EHKB"));
+        WebElement el9= wd.findElement(By.xpath("//*[@class='login_login__3EHKB']"));
+
+
+        WebElement el10= wd.findElement(By.id("root"));
+        WebElement el11= wd.findElement(By.cssSelector("#root"));
+        WebElement el12= wd.findElement(By.xpath("//*[@id='root']"));
+        WebElement a3= wd.findElement(By.xpath("//a[3]"));
+
+
+
+
+
 
 
 
